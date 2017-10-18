78fdd638145ace2603f63571dddf24e3d809dd34
Alex Bazhanau <bazhanau@outlook.com>
2017-10-18 22:06:23 +0300

    DealHistory migrated to string merchantId

--- contracts/DealsHistory.sol
+++ contracts/DealsHistory.sol
@@ -5,13 +5,13 @@ import "zeppelin-solidity/contracts/ownership/Contactable.sol";
 
 contract DealsHistory is Contactable{
 
-    address public merchant;
+    string public merchantId;
     address orderProcessor;
     
     event DealCompleted(uint indexed orderId, address indexed clientAddress, uint dealHash);
 
-    function DealsHistory(address _merchant, address _orderProcessor) public {
-        merchant = _merchant;
+    function DealsHistory(string _merchantId, address _orderProcessor) public {
+        merchantId = _merchantId;
         orderProcessor = _orderProcessor;
     }
 
