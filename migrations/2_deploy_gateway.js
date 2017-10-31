014ff5f509c052f907a5802087ac814588508f35
Alex Bazhanau <bazhanau@outlook.com>
2017-11-01 00:02:02 +0300

    PaymentAcceptor tests added

--- migrations/2_deploy_gateway.js
+++ migrations/2_deploy_gateway.js
@@ -1,7 +1,7 @@
 const MonethaGateway = artifacts.require("./MonethaGateway.sol");
 
 module.exports = function(deployer, network, accounts) {
-  const vault = accounts[1]
-  const processor = accounts[2]
+  const processor = accounts[1]
+  const vault = accounts[2]
   deployer.deploy(MonethaGateway, vault, processor);
 };
--- migrations/3_deploy_wallet.js
+++ migrations/3_deploy_wallet.js
@@ -1,7 +1,7 @@
 const Wallet = artifacts.require("./MerchantWallet.sol");
 
 module.exports = function(deployer, network, accounts) {
-  const merchant = accounts[1]
-  const processor = accounts[2]
+  const processor = accounts[1]
+  const merchant = accounts[2]
   deployer.deploy(Wallet, merchant, "merchantId", processor);
 };
