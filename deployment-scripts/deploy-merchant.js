1cf7e295e0dfef86ddcf4bc7421fc81fc865d240
Alex Bazhanau <bazhanau@outlook.com>
2017-12-29 17:31:09 +0300

    merchant config in separate file

--- deployment-scripts/deploy-merchant.js
+++ deployment-scripts/deploy-merchant.js
@@ -1,22 +1,26 @@
+const assert = require('assert');
+
 const Wallet = artifacts.require("MerchantWallet");
 const MerchantDealsHistory = artifacts.require("MerchantDealsHistory");
 const PaymentProcessor = artifacts.require("PaymentProcessor");
 
 const config = require("./config.json")
+assert(process.argv.length == 5, "Please specify the path to merchant config json file")
+const merchantConfig = require(process.argv[4])
 
 module.exports = function (callback) {
   new Promise(async () => {
     try {
-      const wallet = await Wallet.new(config.merchantAddress, config.merchantId, config.processorAddress)
+      const wallet = await Wallet.new(merchantConfig.merchantAddress, merchantConfig.merchantId, config.processorAddress)
       await wallet.transferOwnership(config.ownerAddress)
       console.log("Wallet deployed at " + wallet.address)
 
-      const history = await MerchantDealsHistory.new(config.merchantId, config.processorAddress)
+      const history = await MerchantDealsHistory.new(merchantConfig.merchantId, config.processorAddress)
       await history.transferOwnership(config.ownerAddress)
       console.log("MerchantDealsHistory deployed at " + history.address)
 
       const paymentProcessor = await PaymentProcessor.new(
-        config.merchantId,
+        merchantConfig.merchantId,
         history.address,
         config.monethaGatewayAddress,
         config.processorAddress
